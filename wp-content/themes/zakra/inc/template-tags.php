6d70d2a7afcae4f72b1d955a95cd97ba2a8e17ca
Lucas Kelim Thiel <lucas.kthiel@gmail.com>
2025-01-25 00:32:35 -0300

    Remoção do link para perfil do dono da postagem

--- wp-content/themes/zakra/inc/template-tags.php
+++ wp-content/themes/zakra/inc/template-tags.php
@@ -163,7 +163,7 @@ if ( ! function_exists( 'zakra_posted_by' ) ) :
 
 		$byline = sprintf(
 			$author_text,
-			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+			'<span class="author vcard"><a class="url fn n">' . esc_html( get_the_author() ) . '</a></span>'
 		);
 
 		echo '<span class="zak-byline"> ' . $catgories_icon . $byline . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
