5cb27bc86e095a9c369f1f3cb7a41572b8d1736e
Lucas Kelim Thiel <lucas.kthiel@gmail.com>
2025-01-25 00:19:16 -0300

    Ajustando separadores

--- wp-content/themes/zakra/template-parts/entry/entry-header.php
+++ wp-content/themes/zakra/template-parts/entry/entry-header.php
@@ -21,10 +21,8 @@ defined( 'ABSPATH' ) || exit;
 			}
 		} else {
 
-			the_title(
-				'<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">',
-				'</a></h2>'
-			);
+			the_title( '<h2 class="entry-title">', '</h2>' );
+			
 		}
 		?>
 	</header> <!-- .zak-entry-header -->
